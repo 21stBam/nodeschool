145cd74e387405184e56af2dca0dfa96dd02ad95
Obsa Aba-waji <oabawaji@gmail.com>
2018-01-28 13:21:17 -0500

    finished lesson 4

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -4,7 +4,15 @@
 //console.log(`Hello, ${name}!
 //Your name lowercased is "${name.toLowerCase()}".`);
 
-var inputs = process.argv.slice(2);
-var results = inputs.map( x => x.charAt(0))
-                    .reduce( (x, y) => x + y);
-console.log(`[${inputs}] becomes "${results}"`);
+//var inputs = process.argv.slice(2);
+//var results = inputs.map( x => x.charAt(0))
+//                    .reduce( (x, y) => x + y);
+//console.log(`[${inputs}] becomes "${results}"`);
+
+var foot = {
+    kick: function () {
+        this.yelp = "Ouch!";
+        setImmediate(() => console.log(this.yelp));
+    }
+};
+foot.kick();
