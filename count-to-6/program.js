db23dcb3f5ccbf23a7986d1325b7771cd49fe5c9
Obsa Aba-waji <oabawaji@gmail.com>
2018-01-28 13:12:47 -0500

    finished lesson 3

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -1,6 +1,10 @@
 //console.log("HELLO ES6");
-let name = process.argv[2];
-console.log(
-`Hello, ${name}!
-Your name lowercased is "${name.toLowerCase()}".`
-);
+
+//let name = process.argv[2];
+//console.log(`Hello, ${name}!
+//Your name lowercased is "${name.toLowerCase()}".`);
+
+var inputs = process.argv.slice(2);
+var results = inputs.map( x => x.charAt(0))
+                    .reduce( (x, y) => x + y);
+console.log(`[${inputs}] becomes "${results}"`);
