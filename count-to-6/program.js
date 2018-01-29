9af1e24a7acd4cd4a0ee489c90af8b8eb3527bad
Obsa Aba-waji <oabawaji@gmail.com>
2018-01-29 00:01:56 -0500

    finished lesson 8

--- count-to-6/program.js
+++ count-to-6/program.js
@@ -22,6 +22,37 @@
 //[, result.username, result.email] = userArray;
 //console.log(result);
 
-let args = process.argv.slice(2);
-let min = Math.min(...args);
-console.log(`The minimum of [${args}] is ${min}`);
+//let args = process.argv.slice(2);
+//let min = Math.min(...args);
+//console.log(`The minimum of [${args}] is ${min}`);
+
+//module.exports = function average(...numbers) {
+//  let result = 0;	
+//  for(let i = 0; i < numbers.length; i++) {
+//    result += numbers[i];
+//   }
+//  return result / numbers.length;
+//};
+
+module.exports = function midpoint(lower = 0, upper = 1) {
+  return (lower + upper) / 2;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
